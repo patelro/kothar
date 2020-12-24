8312bdd61282f6d04920c0ce921ee8636b6bae9c
Chinmay Patel <chinmayp@my.yorku.ca>
2020-12-23 21:02:56 -0500

    Added table list filtering

--- public/TableList.js
+++ public/TableList.js
@@ -91,3 +91,66 @@ db.collection("Items")
             TotalItemList(doc);
         });
     });
+
+function filterInputDate() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("table-filter-inputDate");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("inventoryTable");
+    tr = table.getElementsByTagName("tr");
+
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
+
+function filterProductType() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("table-filter-productType");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("inventoryTable");
+    tr = table.getElementsByTagName("tr");
+
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[2];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
+
+function filterProductID() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("table-filter-productID");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("inventoryTable");
+    tr = table.getElementsByTagName("tr");
+
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[1];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
